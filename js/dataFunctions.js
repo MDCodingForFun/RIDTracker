407cf3a35c42fb721e2141d01d4257525b70ad93
May Deypalubos <mayanndeypalubos@gmail.com>
2022-04-14 09:25:42 +0800

    updated search result box

--- js/dataFunctions.js
+++ js/dataFunctions.js
@@ -3,31 +3,23 @@ export const getSearchTerm = () => {
     const regex = /[]{2,}/gi;
     const searchTerm = rawSearchTerm.replaceAll(regex, "")
     return searchTerm;
-    // console.log(searchTerm);
 };
 
-// export const retrieveSearchResult = async (searchTerm) => {
-//     const foundRID = getRID(searchTerm);
-//     let resultArray = [];
-//     console.log(resultArray);
-//     if (foundRID.hasOwnProperty("RID")) {
-//         resultArray = processRIDResult (foundRID.RID);
-//     }
-//     return resultArray;
-// }
+
 const searchResult = document.getElementById("searchContent")
 export const retrieveSearchResult = async (searchTerm) => {
     const res = await fetch('https://script.google.com/macros/s/AKfycby_gnGan8rqByR8QPJ4klh384tLoW8uCwvBX7-Et5ZO_OOSNGOn3aoQaWHaNU0hdzoq/exec');
     const data = await res.json();
-    // console.log(data);
     
     
     let match = data.find(item => item.RID === searchTerm);
-    //  console.log(match.hasOwnProperty("RID"));
         if (match === undefined) {
             console.log("Not found");
             document.getElementById("searchContent").innerHTML += `
-            <p>No result found for ${searchTerm}.</p>`;
+            <div class="rows row1">
+                <p>No match found for Transaction Number:</p>
+                <p>${searchTerm}</p>
+            </div>`;
         }
         else {
             outputHtml(match);
@@ -38,9 +30,18 @@ export const retrieveSearchResult = async (searchTerm) => {
 const outputHtml = (match) => {
      console.log(typeof match);
      document.getElementById("searchContent").innerHTML += `
-            <p>Result found for Transaction number ${match.RID}.</p>
-            <p>You may claim your National ID at: </p>
-            <p>${match.Location}.</p>
-            <p>Bring any of the documents/ID listed <a href="./docsList.html" target="_blank"> HERE </a>.`;
+                <div class="rows row1">
+                    <p>Result found for Transaction Number:</p>
+                    <p>${match.RID}</p>
+                </div>
+                <div class="rows row2">
+                    <p>You may claim your Phil ID (National ID) at: </p>
+                    <p>${match.Location}</p>
+                </div>
+                <div class="rows row3">
+                    <p>
+                    Bring your transaction slip and any of the IDs/Documents listed <a href="docsList.html" target="blank">HERE.</a>
+                    </p>
+                </div>`;
 
 };
